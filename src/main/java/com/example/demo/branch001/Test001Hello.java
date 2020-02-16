318ed7d94b3eca9a829d78eb4b9660bdcb8aac4a
sunzhichao <826812584@qq.com>
2020-02-16 20:30:30 +0800

    [测试Git]- dev 分支提交

---
+++ src/main/java/com/example/demo/branch001/Test001Hello.java
@@ -0,0 +1,13 @@
+package com.example.demo.branch001;
+
+/**
+ * @Class: Test001Hello
+ * @Description:
+ * @Author: sunzhichao
+ * @Date: 2020/2/16 20:28
+ */
+public class Test001Hello {
+    public static void main(String[] args) {
+        System.out.println("This is Test001Hello.java");
+    }
+}
