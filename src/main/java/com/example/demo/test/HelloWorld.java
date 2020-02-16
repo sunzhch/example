d844b088f896e4ce456023f8ef38291e436eb063
sunzhichao <826812584@qq.com>
2020-02-16 18:18:04 +0800

    [测试Git]- 第一次提交 hello world

---
+++ src/main/java/com/example/demo/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.demo.test;
+
+/**
+ * @class: HelloWorld
+ * @description:
+ * @author: sunzhichao
+ * @date: 2020/2/16 18:16
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
