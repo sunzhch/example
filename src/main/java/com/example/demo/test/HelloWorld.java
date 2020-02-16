c543a947356483fb81b07a6876704292bb6d5d97
sunzhichao <826812584@qq.com>
2020-02-16 20:48:41 +0800

    [测试Git]- dev 分支提交 java8 clock
    (cherry picked from commit 9346936)

--- src/main/java/com/example/demo/test/HelloWorld.java
+++ src/main/java/com/example/demo/test/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.example.demo.test;
 
+import java.time.Clock;
 import java.time.LocalDate;
 
 /**
@@ -14,6 +15,15 @@ public class HelloWorld {
 
         LocalDate localDate = LocalDate.now();
         System.out.println("今天的日期是：" + localDate);
+
+        LocalDate date = LocalDate.of(2020, 02, 16);
+        System.out.println("date 的时间是:" + date);
+        // 获取当前时间
+        Clock clock = Clock.systemUTC();
+        System.out.println("当前时刻为：" + clock.instant());
+        System.out.println("当前时刻1 :" + clock.millis());
+        System.out.println("当前时刻2 :" + System.currentTimeMillis());
+
 
     }
 }
